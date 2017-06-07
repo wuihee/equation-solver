16a7caf2e4c768e231c078cf9a45fd60b2cff73c
Wuihee <wuihee@gmail.com>
2017-06-07 08:58:40 -0700

    Removed Equation.java and updated equation functionality

--- src/EquationSolver.java
+++ src/EquationSolver.java
@@ -1,15 +1,22 @@
 import java.util.*;
+import javax.script.*;
 
 public class EquationSolver {
-    public static void main(String[] args) {
-        ArrayList<String> equation = Equation.getEquation();
-        if (equation.isEmpty()) {
-            System.out.println("No Equation");
-        } else {
-            System.out.print("f(x) = ");
-            for (String t : equation) {
-                System.out.print(t);
-            }
-        }
+    public static void main(String[] args) throws ScriptException {
+        Scanner console = new Scanner(System.in);
+        String equation = getEquation(console);
+        System.out.println(equation);
+        evalString("1 + 1 + 10 * 100");
+    }
+
+    public static String getEquation(Scanner console) {
+        System.out.print("Enter Equation (Use Whitespace): ");
+        return "y = " + console.nextLine();
+    }
+
+    public static void evalString(String expression) throws ScriptException {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        System.out.println(engine.eval(expression));
     }
 }
