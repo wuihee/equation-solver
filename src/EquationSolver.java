4e56b4c8ed904a2cda10a4660f8791cd573031df
Wuihee <wuihee@gmail.com>
2017-06-07 09:33:42 -0700

    add points with equation

--- src/EquationSolver.java
+++ src/EquationSolver.java
@@ -4,9 +4,23 @@ import javax.script.*;
 public class EquationSolver {
     public static void main(String[] args) throws ScriptException {
         Scanner console = new Scanner(System.in);
-        String equation = getEquation(console);
-        System.out.println(equation);
-        evalString("1 + 1 + 10 * 100");
+        int[] points1 = getPoints(getEquation(console), 1, 10);
+        int[] points2 = getPoints(getEquation(console), -10, 20);
+    }
+
+    public static int[] getPoints(String equation, int start, int end) throws ScriptException {
+        int[] points = new int[Math.abs(start - end) + 1];
+        int count = 0;
+
+        for (int i = start; i < end; i++) {
+            String coefficient = " * " + i;
+            String expression = equation.replace("x", coefficient);
+            System.out.println(expression);
+            points[count] = evalString(expression);
+            count++;
+        }
+
+        return points;
     }
 
     public static String getEquation(Scanner console) {
@@ -14,9 +28,9 @@ public class EquationSolver {
         return "y = " + console.nextLine();
     }
 
-    public static void evalString(String expression) throws ScriptException {
+    public static int evalString(String expression) throws ScriptException {
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine engine = mgr.getEngineByName("JavaScript");
-        System.out.println(engine.eval(expression));
+        return (int) engine.eval(expression);
     }
 }
