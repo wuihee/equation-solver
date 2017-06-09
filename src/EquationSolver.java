3136fd862ea6ffda9fd190360e2834e2d147f5c3
Wuihee <wuihee@gmail.com>
2017-06-09 08:48:27 -0700

    updated EquationSolver

--- src/EquationSolver.java
+++ src/EquationSolver.java
@@ -5,30 +5,32 @@ public class EquationSolver {
     public static void main(String[] args) throws ScriptException {
         Scanner console = new Scanner(System.in);
         int[] points1 = getPoints(getEquation(console), 1, 10);
-        int[] points2 = getPoints(getEquation(console), -10, 20);
+        System.out.println(Arrays.toString(points1));
     }
 
-    public static int[] getPoints(String equation, int start, int end) throws ScriptException {
-        int[] points = new int[Math.abs(start - end) + 1];
+    private static int[] getPoints(String equation, int start, int end) throws ScriptException {
+        int len = Math.abs(start - end) + 1;
+        int[] xPoints = new int[len];
+        int[] yPoints = new int[len];
         int count = 0;
 
         for (int i = start; i < end; i++) {
             String coefficient = " * " + i;
             String expression = equation.replace("x", coefficient);
-            System.out.println(expression);
-            points[count] = evalString(expression);
+            xPoints[count] = evalString(expression);
+            yPoints[count] = i;
             count++;
         }
 
-        return points;
+        return xPoints;
     }
 
-    public static String getEquation(Scanner console) {
+    private static String getEquation(Scanner console) {
         System.out.print("Enter Equation (Use Whitespace): ");
         return "y = " + console.nextLine();
     }
 
-    public static int evalString(String expression) throws ScriptException {
+    private static int evalString(String expression) throws ScriptException {
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine engine = mgr.getEngineByName("JavaScript");
         return (int) engine.eval(expression);
