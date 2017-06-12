34e84080b1ceae4709b062cfb4e2333f6e1dea03
wuihee <wuihee@gmail.com>
2017-06-11 23:00:04 -0700

    updated KeypadTest

--- src/KeypadTest.java
+++ src/KeypadTest.java
@@ -1,11 +1,13 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 import javax.swing.*;
 
 class KeypadTest implements ActionListener {
   JFrame frame;
   JPanel contentPane;
   JButton button;
+  ArrayList<JButton> buttons;
 
   KeypadTest() {
     frame = new JFrame("Keypad");
@@ -16,14 +18,35 @@ class KeypadTest implements ActionListener {
 
     button = new JButton("Click Me!");
     button.addActionListener(this);
+    addButtons();
 
     contentPane = new JPanel();  // Use default FlowLayout.
     contentPane.add(button);
+    for (JButton b : buttons) {
+      contentPane.add(b);
+    }
     frame.setContentPane(contentPane);
   }
 
+  public void addButtons() {
+    buttons = new ArrayList<JButton>();
+
+    for (int i = 0; i <= 3; i++) {
+      buttons.add(new JButton(Integer.toString(i)));
+      buttons.get(i).addActionListener(this);
+    }
+  }
+
   public void actionPerformed(ActionEvent e) {
-    System.out.println("Yay!");
+    if (e.getSource() == button) {
+      System.out.println("First Manual Button");
+    }
+
+    for (JButton b : buttons) {
+      if (e.getSource() == b) {
+        System.out.println(b.getText());
+      }
+    }
   }
 
   public static void main(String[] args) {
