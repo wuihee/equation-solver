876c0cf5962e753dd7481e9062f44240d5b9cc4f
wuihee <wuihee@gmail.com>
2017-06-19 09:24:42 -0700

    Updated comments

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,6 @@
 public class Client {
   public static void main(String[] args) {
     Keypad keypad = new Keypad();
-    keypad.createAndShowGUI();
-    // comment
+    keypad.createAndShowGUI();  // Create a Keypad object and show GUI.
   }
 }
--- src/Keypad.java
+++ src/Keypad.java
@@ -20,11 +20,12 @@ public class Keypad implements ActionListener {
 
   private void getFrame() {
     /* Initialize main frame, with its properties. */
-    frame = new JFrame("Keypad");
-    frame.setLocationRelativeTo(null);
+    frame = new JFrame("Graphing Calculator");
+    frame.setLocationRelativeTo(null);  // Makes components appear in the middle of the frame.
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
     frame.setSize(1000, 1200);
+    frame.setResizable(true);  // Make the frame resizable (so that different users can use it).
   }
 
   private ArrayList<JButton> getButtonSection(String[] buttonNames) {
@@ -33,6 +34,7 @@ public class Keypad implements ActionListener {
     Font buttonFont = new Font("Arial", Font.PLAIN, 25);
 
     for (String name : buttonNames) {
+      // Set the font, ActionListener and add each button.
        JButton button = new JButton(name);
        button.setFont(buttonFont);
        button.addActionListener(this);
@@ -42,17 +44,19 @@ public class Keypad implements ActionListener {
   }
 
   private JPanel addButtonSection(JPanel contentPane, ArrayList<JButton> buttons, int numCols, int numRows, int gridx, int gridy) {
-    GridBagConstraints constraints = new GridBagConstraints();
+    /* Add a set of buttons in an ArrayList. */
+    GridBagConstraints constraints = new GridBagConstraints();  // Set component constraints using this object.
     for (int i = 0; i < buttons.size(); i++) {
       int col = i % numCols;  // To find current column (x value).
       int row = i / numCols;  // To find current row (y value).
       JButton b = buttons.get(i);
-      constraints.gridx = col + gridx;
+      constraints.gridx = col + gridx;  // x and y position of the section.
       constraints.gridy = row + gridy;
-      constraints.fill = GridBagConstraints.BOTH;
-      constraints.ipadx = 60;
+      constraints.fill = GridBagConstraints.BOTH;  // Fill unfilled spaces.
+      constraints.ipadx = 60;  // Size of the buttons.
       constraints.ipady = 60;
       // Create algorithm to evenly space all buttons.
+      // Current way of spacing buttons is extremely inconsistent and repetitive.
       // constraints.insets = new Insets(5, 5, 5, 5);
       contentPane.add(b, constraints);
     }
@@ -60,18 +64,19 @@ public class Keypad implements ActionListener {
   }
 
   private JPanel addKeypadScreen(JPanel contentPane, int width, int gridx, int gridy) {
-    GridBagConstraints constraints = new GridBagConstraints();
+    /* Creates and adds the screen of the keypad where button inputs show up. */
+    GridBagConstraints constraints = new GridBagConstraints();  // Set component constraints using this object.
     Font screenFont = new Font("Arial", Font.PLAIN, 30);
     keypadScreen = new JTextField(20);
 
-    keypadScreen.setEditable(false);
+    keypadScreen.setEditable(false);  // Does not allow users to edit.
     keypadScreen.setFont(screenFont);
-    keypadScreen.setText("y=");
+    keypadScreen.setText("y=");  // Because it is a graphing calculator, always start with "y=".
     constraints.gridx = gridx;
     constraints.gridy = gridy;
-    constraints.gridwidth = width;
-    constraints.fill = GridBagConstraints.BOTH;
-    constraints.ipadx = 150;
+    constraints.gridwidth = width;  // Stretch across the width of the buttons.
+    constraints.fill = GridBagConstraints.BOTH;  // Fill unfilled spaces.
+    constraints.ipadx = 150;  // Set padding.
     constraints.ipady = 50;
     contentPane.add(keypadScreen, constraints);
     return contentPane;
@@ -79,7 +84,7 @@ public class Keypad implements ActionListener {
 
   private void addComponents() {
     /* Adds all components to the contents pane. */
-    int numCols = 5;
+    int numCols = 5;  // Calculator is 5 x 4.
     int numRows = 4;
     keypadButtons = getButtonSection(buttonNames);
 
@@ -93,7 +98,7 @@ public class Keypad implements ActionListener {
 
   private void updateScreen() {
     /* Updates equation on keypad screen with new terms and operations. */
-    String text = "y=";
+    String text = "y=";  // Ensures that start is always "y=".
     text += equation.getEquation() + equation.getTerm();
     keypadScreen.setText(text);
   }
@@ -107,7 +112,7 @@ public class Keypad implements ActionListener {
     for (JButton b : keypadButtons) {
       if (event.getSource() == b) {
         try {
-          equation = KeypadInput.updateEquation(equation, b.getText());
+          equation = KeypadInput.updateEquation(equation, b.getText());  // Update Equation object using KeypadInput.
         }
         catch (ScriptException e) {
           System.out.println(e);
